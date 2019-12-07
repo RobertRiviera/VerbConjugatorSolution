c10fe78cfa6aa8499cf4705a5993851ed568f265
Robert Riviera <ber@harriergroup.com>
2019-12-06 19:32:01 -0500

    Initial commit

---
+++ VerbConjugator.Test/UnitTest1.cs
@@ -0,0 +1,257 @@
+using Language.Spanish;
+using Xunit;
+
+namespace VerbConjugator.Test
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void TestPresentTense_AR()
+		{
+			Verb verb = new Verb("cantar");
+			Tense tense = Tense.Present;
+			
+			Assert.Equal("canto", verb.Conjugate(tense, Person.Yo));
+			Assert.Equal("cantas", verb.Conjugate(tense, Person.Tú));
+			Assert.Equal("canta", verb.Conjugate(tense, Person.Él));
+			Assert.Equal("cantamos", verb.Conjugate(tense, Person.Nosotros));
+			Assert.Equal("cantáis", verb.Conjugate(tense, Person.Vosotros));
+			Assert.Equal("cantan", verb.Conjugate(tense, Person.Ellos));
+		}
+
+		[Fact]
+		public void TestPresentTense_ER()
+		{
+			Verb verb = new Verb("aprender");
+			Tense tense = Tense.Present;
+
+			Assert.Equal("aprendo", verb.Conjugate(tense, Person.Yo));
+			Assert.Equal("aprendes", verb.Conjugate(tense, Person.Tú));
+			Assert.Equal("aprende", verb.Conjugate(tense, Person.Él));
+			Assert.Equal("aprendemos", verb.Conjugate(tense, Person.Nosotros));
+			Assert.Equal("aprendéis", verb.Conjugate(tense, Person.Vosotros));
+			Assert.Equal("aprenden", verb.Conjugate(tense, Person.Ellos));
+		}
+
+		[Fact]
+		public void TestPresentTense_IR()
+		{
+			Verb verb = new Verb("abrir");
+			Tense tense = Tense.Present;
+
+			Assert.Equal("abro", verb.Conjugate(tense, Person.Yo));
+			Assert.Equal("abres", verb.Conjugate(tense, Person.Tú));
+			Assert.Equal("abre", verb.Conjugate(tense, Person.Él));
+			Assert.Equal("abrimos", verb.Conjugate(tense, Person.Nosotros));
+			Assert.Equal("abrís", verb.Conjugate(tense, Person.Vosotros));
+			Assert.Equal("abren", verb.Conjugate(tense, Person.Ellos));
+		}
+
+		[Fact]
+		public void TestPreteriteTense_AR()
+		{
+			Verb verb = new Verb("hablar");
+			Tense tense = Tense.Preterite;
+
+			Assert.Equal("hablé", verb.Conjugate(tense, Person.Yo));
+			Assert.Equal("hablaste", verb.Conjugate(tense, Person.Tú));
+			Assert.Equal("habló", verb.Conjugate(tense, Person.Él));
+			Assert.Equal("hablamos", verb.Conjugate(tense, Person.Nosotros));
+			Assert.Equal("hablasteis", verb.Conjugate(tense, Person.Vosotros));
+			Assert.Equal("hablaron", verb.Conjugate(tense, Person.Ellos));
+		}
+
+		[Fact]
+		public void TestPreteriteTense_ER()
+		{
+			Verb verb = new Verb("aprender");
+			Tense tense = Tense.Preterite;
+
+			Assert.Equal("aprendí", verb.Conjugate(tense, Person.Yo));
+			Assert.Equal("aprendiste", verb.Conjugate(tense, Person.Tú));
+			Assert.Equal("aprendió", verb.Conjugate(tense, Person.Él));
+			Assert.Equal("aprendimos", verb.Conjugate(tense, Person.Nosotros));
+			Assert.Equal("aprendisteis", verb.Conjugate(tense, Person.Vosotros));
+			Assert.Equal("aprendieron", verb.Conjugate(tense, Person.Ellos));
+		}
+
+		[Fact]
+		public void TestPreteriteTense_IR()
+		{
+			Verb verb = new Verb("sufrir");
+			Tense tense = Tense.Preterite;
+
+			Assert.Equal("sufrí", verb.Conjugate(tense, Person.Yo));
+			Assert.Equal("sufriste", verb.Conjugate(tense, Person.Tú));
+			Assert.Equal("sufrió", verb.Conjugate(tense, Person.Él));
+			Assert.Equal("sufrimos", verb.Conjugate(tense, Person.Nosotros));
+			Assert.Equal("sufristeis", verb.Conjugate(tense, Person.Vosotros));
+			Assert.Equal("sufrieron", verb.Conjugate(tense, Person.Ellos));
+		}
+
+		[Fact]
+		public void TestImperfectTense_AR()
+		{
+			Verb verb = new Verb("bailar");
+			Tense tense = Tense.Imperfect;
+
+			Assert.Equal("bailaba", verb.Conjugate(tense, Person.Yo));
+			Assert.Equal("bailabas", verb.Conjugate(tense, Person.Tú));
+			Assert.Equal("bailaba", verb.Conjugate(tense, Person.Él));
+			Assert.Equal("bailábamos", verb.Conjugate(tense, Person.Nosotros));
+			Assert.Equal("bailabais", verb.Conjugate(tense, Person.Vosotros));
+			Assert.Equal("bailaban", verb.Conjugate(tense, Person.Ellos));
+		}
+
+		[Fact]
+		public void TestImperfectTense_ER()
+		{
+			Verb verb = new Verb("vender");
+			Tense tense = Tense.Imperfect;
+
+			Assert.Equal("vendía", verb.Conjugate(tense, Person.Yo));
+			Assert.Equal("vendías", verb.Conjugate(tense, Person.Tú));
+			Assert.Equal("vendía", verb.Conjugate(tense, Person.Él));
+			Assert.Equal("vendíamos", verb.Conjugate(tense, Person.Nosotros));
+			Assert.Equal("vendíais", verb.Conjugate(tense, Person.Vosotros));
+			Assert.Equal("vendían", verb.Conjugate(tense, Person.Ellos));
+		}
+
+		[Fact]
+		public void TestImperfectTense_IR()
+		{
+			Verb verb = new Verb("vivir");
+			Tense tense = Tense.Imperfect;
+
+			Assert.Equal("vivía", verb.Conjugate(tense, Person.Yo));
+			Assert.Equal("vivías", verb.Conjugate(tense, Person.Tú));
+			Assert.Equal("vivía", verb.Conjugate(tense, Person.Él));
+			Assert.Equal("vivíamos", verb.Conjugate(tense, Person.Nosotros));
+			Assert.Equal("vivíais", verb.Conjugate(tense, Person.Vosotros));
+			Assert.Equal("vivían", verb.Conjugate(tense, Person.Ellos));
+		}
+
+		[Fact]
+		public void TestConditionalTense_AR()
+		{
+			Verb verb = new Verb("cantar");
+			Tense tense = Tense.Conditional;
+
+			Assert.Equal("cantaría", verb.Conjugate(tense, Person.Yo));
+			Assert.Equal("cantarías", verb.Conjugate(tense, Person.Tú));
+			Assert.Equal("cantaría", verb.Conjugate(tense, Person.Él));
+			Assert.Equal("cantaríamos", verb.Conjugate(tense, Person.Nosotros));
+			Assert.Equal("cantaríais", verb.Conjugate(tense, Person.Vosotros));
+			Assert.Equal("cantarían", verb.Conjugate(tense, Person.Ellos));
+		}
+
+		[Fact]
+		public void TestConditionalTense_ER()
+		{
+			Verb verb = new Verb("conocer");
+			Tense tense = Tense.Conditional;
+
+			Assert.Equal("conocería", verb.Conjugate(tense, Person.Yo));
+			Assert.Equal("conocerías", verb.Conjugate(tense, Person.Tú));
+			Assert.Equal("conocería", verb.Conjugate(tense, Person.Él));
+			Assert.Equal("conoceríamos", verb.Conjugate(tense, Person.Nosotros));
+			Assert.Equal("conoceríais", verb.Conjugate(tense, Person.Vosotros));
+			Assert.Equal("conocerían", verb.Conjugate(tense, Person.Ellos));
+		}
+
+		[Fact]
+		public void TestConditionalTense_IR()
+		{
+			Verb verb = new Verb("vivir");
+			Tense tense = Tense.Conditional;
+
+			Assert.Equal("viviría", verb.Conjugate(tense, Person.Yo));
+			Assert.Equal("vivirías", verb.Conjugate(tense, Person.Tú));
+			Assert.Equal("viviría", verb.Conjugate(tense, Person.Él));
+			Assert.Equal("viviríamos", verb.Conjugate(tense, Person.Nosotros));
+			Assert.Equal("viviríais", verb.Conjugate(tense, Person.Vosotros));
+			Assert.Equal("vivirían", verb.Conjugate(tense, Person.Ellos));
+		}
+
+		[Fact]
+		public void TestFutureTense_AR()
+		{
+			Verb verb = new Verb("tratar");
+			Tense tense = Tense.Future;
+
+			Assert.Equal("trataré", verb.Conjugate(tense, Person.Yo));
+			Assert.Equal("tratarás", verb.Conjugate(tense, Person.Tú));
+			Assert.Equal("tratará", verb.Conjugate(tense, Person.Él));
+			Assert.Equal("trataremos", verb.Conjugate(tense, Person.Nosotros));
+			Assert.Equal("trataréis", verb.Conjugate(tense, Person.Vosotros));
+			Assert.Equal("tratarán", verb.Conjugate(tense, Person.Ellos));
+		}
+
+		[Fact]
+		public void TestFutureTense_ER()
+		{
+			Verb verb = new Verb("correr");
+			Tense tense = Tense.Future;
+
+			Assert.Equal("correré", verb.Conjugate(tense, Person.Yo));
+			Assert.Equal("correrás", verb.Conjugate(tense, Person.Tú));
+			Assert.Equal("correrá", verb.Conjugate(tense, Person.Él));
+			Assert.Equal("correremos", verb.Conjugate(tense, Person.Nosotros));
+			Assert.Equal("correréis", verb.Conjugate(tense, Person.Vosotros));
+			Assert.Equal("correrán", verb.Conjugate(tense, Person.Ellos));
+		}
+
+
+		[Theory]
+		[InlineData("hablar")]
+		[InlineData("cantar")]
+		[InlineData("llorar")]
+		public void IsVerbType_AR(string v)
+		{
+			Verb verb = new Verb(v);
+
+			var result = verb.Type == "AR";
+
+			Assert.True(result);
+		}
+
+
+		[Theory]
+		[InlineData("aprender")]
+		public void IsVerbType_ER(string v)
+		{
+			Verb verb = new Verb(v);
+
+			var result = verb.Type == "ER";
+
+			Assert.True(result);
+		}
+
+
+		[Theory]
+		[InlineData("hablar")]
+		[InlineData("cantar")]
+		[InlineData("llorar")]
+		[InlineData("tener")]
+		[InlineData("venir")]
+		public void TestValidInfinitive(string v)
+		{
+			Assert.True(Verb.IsValidInfinitive(v));
+		}
+
+
+		[Theory]
+		[InlineData("")]
+		[InlineData("skank")]
+		[InlineData("horror")]
+		[InlineData(null)]
+		[InlineData("xxx")]
+		[InlineData("arararx")]
+		[InlineData("a90210")]
+		[InlineData(")*#FM*#$*#&@")]
+		public void TestInvalidInfinitive(string v)
+		{
+			Assert.False(Verb.IsValidInfinitive(v));
+		}
+	}
+}
---
+++ VerbConjugator/Pages/Verb.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace Language.Spanish
+{
+	public enum VerbType
+	{
+		AR,
+		ER,
+		IR
+	}
+
+	public enum Tense
+	{
+		Present,
+		Preterite,
+		Imperfect,
+		Conditional,
+		Future
+	}
+
+	public enum Person
+	{
+		Yo,
+		Tú,
+		Él,
+		Nosotros,
+		Vosotros,
+		Ellos
+	}
+
+	public enum Mood
+	{
+		Indicative,
+		Imperative,
+		Subjunctive,
+	}
+
+
+	public class Verb
+	{
+		public string Infinitive { get; set; }
+
+		public string Root { get => Infinitive[..^2]; }
+
+		public string Type { get => Infinitive[^2..].ToUpper(); }
+
+		public Verb(string infinitive)
+		{
+			if (!IsValidInfinitive(infinitive))
+				throw new InvalidOperationException("Bad infinitive.");
+
+			Infinitive = infinitive.ToLowerInvariant();
+		}
+		
+
+		public static bool IsValidInfinitive(string s)
+		{
+			if (s == null || s.Length < 2)
+				return false;
+
+			return s[^2..].ToUpper() switch
+			{
+				"AR" => true,
+				"IR" => true,
+				"ER" => true,
+				_ => false
+			};			
+		}
+
+
+		public string Conjugate(Tense tense, Person person, Mood mood = Mood.Indicative)
+		{
+			if(tense == Tense.Future)
+				return person switch
+				{
+					Person.Yo => Infinitive + "é",
+					Person.Tú => Infinitive + "ás",
+					Person.Él => Infinitive + "á",
+					Person.Nosotros => Infinitive + "emos",
+					Person.Vosotros => Infinitive + "éis",
+					Person.Ellos => Infinitive + "án",
+					_ => ""
+				};
+			
+			return
+			Type switch
+			{
+				"AR" =>
+					tense switch
+					{
+						Tense.Present => person switch
+						{
+							Person.Yo => Root + "o",
+							Person.Tú => Root + "as",
+							Person.Él => Root + "a",
+							Person.Nosotros => Root + "amos",
+							Person.Vosotros => Root + "áis",
+							Person.Ellos => Root + "an",
+							_ => ""
+						},
+
+						Tense.Preterite => person switch
+						{
+							Person.Yo => Root + "é",
+							Person.Tú => Root + "aste",
+							Person.Él => Root + "ó",
+							Person.Nosotros => Root + "amos",
+							Person.Vosotros => Root + "asteis",
+							Person.Ellos => Root + "aron",
+							_ => ""
+						},
+
+						Tense.Imperfect => person switch
+						{
+							Person.Yo => Root + "aba",
+							Person.Tú => Root + "abas",
+							Person.Él => Root + "aba",
+							Person.Nosotros => Root + "ábamos",
+							Person.Vosotros => Root + "abais",
+							Person.Ellos => Root + "aban",
+							_ => ""
+						},
+
+						Tense.Conditional => person switch          // Sames as IR imperfect!!
+						{
+							Person.Yo => Infinitive + "ía",
+							Person.Tú => Infinitive + "ías",
+							Person.Él => Infinitive + "ía",
+							Person.Nosotros => Infinitive + "íamos",
+							Person.Vosotros => Infinitive + "íais",
+							Person.Ellos => Infinitive + "ían",
+							_ => ""
+						},
+
+						//Tense.Future => person switch
+						//{
+						//	Person.Yo => Infinitive + "é",
+						//	Person.Tú => Infinitive + "ás",
+						//	Person.Él => Infinitive + "á",
+						//	Person.Nosotros => Infinitive + "emos",
+						//	Person.Vosotros => Infinitive + "éis",
+						//	Person.Ellos => Infinitive + "án",
+						//	_ => ""
+						//},
+						_ => ""
+					},
+
+				"ER" =>
+					tense switch
+					{
+						Tense.Present => person switch
+						{
+							Person.Yo => Root + "o",
+							Person.Tú => Root + "es",
+							Person.Él => Root + "e",
+							Person.Nosotros => Root + "emos",
+							Person.Vosotros => Root + "éis",
+							Person.Ellos => Root + "en",
+							_ => ""
+						},
+
+						Tense.Preterite => person switch
+						{
+							Person.Yo => Root + "í",
+							Person.Tú => Root + "iste",
+							Person.Él => Root + "ió",
+							Person.Nosotros => Root + "imos",
+							Person.Vosotros => Root + "isteis",
+							Person.Ellos => Root + "ieron",
+							_ => ""
+						},
+
+						Tense.Imperfect => person switch
+						{
+							Person.Yo => Root + "ía",
+							Person.Tú => Root + "ías",
+							Person.Él => Root + "ía",
+							Person.Nosotros => Root + "íamos",
+							Person.Vosotros => Root + "íais",
+							Person.Ellos => Root + "ían",
+							_ => ""
+						},
+
+						Tense.Conditional => person switch          // Same as Imperfect above, but with infinitive.
+						{
+							Person.Yo => Infinitive + "ía",
+							Person.Tú => Infinitive + "ías",
+							Person.Él => Infinitive + "ía",
+							Person.Nosotros => Infinitive + "íamos",
+							Person.Vosotros => Infinitive + "íais",
+							Person.Ellos => Infinitive + "ían",
+							_ => ""
+						},
+
+						//Tense.Future => person switch               // Same as AR future
+						//{
+						//	Person.Yo => Infinitive + "é",
+						//	Person.Tú => Infinitive + "ás",
+						//	Person.Él => Infinitive + "á",
+						//	Person.Nosotros => Infinitive + "emos",
+						//	Person.Vosotros => Infinitive + "éis",
+						//	Person.Ellos => Infinitive + "án",
+						//	_ => ""
+						//},
+						_ => ""
+					},
+
+				"IR" =>
+					tense switch
+					{
+						Tense.Present => person switch
+						{
+							Person.Yo => Root + "o",
+							Person.Tú => Root + "es",
+							Person.Él => Root + "e",
+							Person.Nosotros => Root + "imos",
+							Person.Vosotros => Root + "ís",
+							Person.Ellos => Root + "en",
+							_ => ""
+						},
+
+						Tense.Preterite => person switch
+						{
+							Person.Yo => Root + "í",
+							Person.Tú => Root + "iste",
+							Person.Él => Root + "ió",
+							Person.Nosotros => Root + "imos",
+							Person.Vosotros => Root + "isteis",
+							Person.Ellos => Root + "ieron",
+							_ => ""
+						},
+
+						Tense.Imperfect => person switch
+						{
+							Person.Yo => Root + "ía",
+							Person.Tú => Root + "ías",
+							Person.Él => Root + "ía",
+							Person.Nosotros => Root + "íamos",
+							Person.Vosotros => Root + "íais",
+							Person.Ellos => Root + "ían",
+							_ => ""
+						},
+
+						Tense.Conditional => person switch          // Same as above but with infinitive
+						{
+							Person.Yo => Infinitive + "ía",
+							Person.Tú => Infinitive + "ías",
+							Person.Él => Infinitive + "ía",
+							Person.Nosotros => Infinitive + "íamos",
+							Person.Vosotros => Infinitive + "íais",
+							Person.Ellos => Infinitive + "ían",
+							_ => ""
+						},
+
+						//Tense.Future => person switch               // Same as AR and ER future
+						//{
+						//	Person.Yo => Infinitive + "é",
+						//	Person.Tú => Infinitive + "ás",
+						//	Person.Él => Infinitive + "á",
+						//	Person.Nosotros => Infinitive + "emos",
+						//	Person.Vosotros => Infinitive + "éis",
+						//	Person.Ellos => Infinitive + "án",
+						//	_ => ""
+						//},
+						_ => ""
+					},
+				_ => ""
+			};
+		}
+	}
+}
